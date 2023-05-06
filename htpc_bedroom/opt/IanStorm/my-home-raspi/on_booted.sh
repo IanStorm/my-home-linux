8371c9fb481e74fe07f42b713c02fa92c5755db2
Christian Sporer <IanStorm28@outlook.com>
2023-05-06 20:27:33 +0200

    (htpc_bedroom) Disable auto-reset audio volume

--- htpc_bedroom/opt/IanStorm/my-home-raspi/on_booted.sh
+++ htpc_bedroom/opt/IanStorm/my-home-raspi/on_booted.sh
@@ -5,12 +5,3 @@
 # ⬇ 	👀 https://stackoverflow.com/a/36853636
 echo 0 | sudo tee /sys/class/leds/PWR/brightness
 echo none | sudo tee /sys/class/leds/PWR/trigger
-
-# ⬇ Set audio volume
-# ⬇ 	👀 https://raspberrypi.stackexchange.com/a/37132
-while amixer sget 'Master' 2>&1 | grep -i -w 'unable to find simple control'
-do
-	sleep 5s
-done
-amixer sset 'Master' 25%
-echo 'Set audio volume to 25%'
