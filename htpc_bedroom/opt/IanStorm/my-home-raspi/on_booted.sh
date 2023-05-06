6fdef43376d81d5d36943fb30d99c57af558ddf2
Christian Sporer <IanStorm28@outlook.com>
2023-05-06 17:33:59 +0200

    (htpc_bedroom) Auto-reset audio volume

--- htpc_bedroom/opt/IanStorm/my-home-raspi/on_booted.sh
+++ htpc_bedroom/opt/IanStorm/my-home-raspi/on_booted.sh
@@ -8,4 +8,9 @@ echo none | sudo tee /sys/class/leds/PWR/trigger
 
 # ⬇ Set audio volume
 # ⬇ 	👀 https://raspberrypi.stackexchange.com/a/37132
+while amixer sget 'Master' 2>&1 | grep -i -w 'unable to find simple control'
+do
+	sleep 5s
+done
 amixer sset 'Master' 25%
+echo 'Set audio volume to 25%'
